12526aefaae82ee885d5a59e6232eb3b65df3fcd
kelah <kelechiekebor@gmail.com>
2021-08-31 21:19:13 +0100

    modified typo

--- src/components/tabContainer/tabContainer.js
+++ src/components/tabContainer/tabContainer.js
@@ -1,11 +1,14 @@
-import React, { Children } from 'react'
+import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const TabContainer = () => {
     return(
-        <ul className="">
-            <li>I am a list</li>
-        </ul>
+        <div>
+            <ul className="">
+                <li>I am a list</li>
+            </ul>
+            {children}
+        </div>
     )
 }
 export {TabContainer}
